b3b014e2659df86af894116065e65a1e3d947a0c
류연경 <ykr.dev@gmail.com>
2025-11-12 20:23:05 +0900

    feat: Car 구조체 추가

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+struct Car {
+    name: String,
+    position: u32
+}
+
+impl Car {
+    fn getPosition(&mut self) -> String {
+        "-".repeat(self.position as usize)
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("경주 할 자동차 이름(이름은 쉼표(,) 기준으로 구분)");
+    println!("시도할 회수는 몇회인가요?");
+    println!("실행 결과");
+    println!("최종 우승자 :");
 }
