f42cef0edfb530c19a730fa0d65df4bf3c42c318
류연경 <ykr.dev@gmail.com>
2025-11-12 20:43:27 +0900

    feat: 시도 횟수 입력 구현

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::io;
+
+// struct Cars {
+//     cars: Vec<Car>
+// }
+//
+// impl Cars {
+//     fn printResult(&mut self) {
+//
+//     }
+//     fn getWinners(&mut self) -> String {
+//
+//     }
+// }
+
 struct Car {
     name: String,
     position: u32
@@ -9,9 +24,29 @@ impl Car {
     }
 }
 
+fn userInput() -> String{
+    let mut userInput = String::new();
+    io::stdin().read_line(&mut userInput).unwrap();
+    userInput
+}
+
+fn getCars() {
+    let userInput = userInput();
+
+}
+
+fn getTryNum() -> u32 {
+    let userInput = userInput();
+    userInput.parse().unwrap()
+}
+
 fn main() {
     println!("경주 할 자동차 이름(이름은 쉼표(,) 기준으로 구분)");
+    // let cars: Vec<Car> = getCars();
+
     println!("시도할 회수는 몇회인가요?");
+    let tryNum = getTryNum();
+
     println!("실행 결과");
     println!("최종 우승자 :");
 }
